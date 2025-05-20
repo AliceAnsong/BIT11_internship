8c45d1d6a811c4930eaa5b063896b140bc8ceaf2
Alice Ansong <189720600+AliceAnsong@users.noreply.github.com>
2025-05-20 13:56:21 +0200

    Fixed issue where all EHdn_STR_profile output was used for manifest.tsv instead of just the json file

--- pipeline/manifest.sh
+++ pipeline/manifest.sh
@@ -1,8 +1,7 @@
- #!/usr/bin/env 
+ #!/usr/bin/env
 
-for i in $(ls results/EHdn_prep/str-profiles/); 
+for i in $(ls results/EHdn_prep/str-profiles/*.json); 
 do 
-    FILE=results/EHdn_prep/str-profiles/$i
-    SAMPLE=$(echo "$i" | sed -E 's/^(.*)\.str_profile\.json$/\1/')
-    echo -e "$SAMPLE\tcase\t$FILE" >> "results/EHdn_prep/manifest.tsv"
+    SAMPLE=$(basename "$i" | sed -E 's/^(.*)\.str_profile\.json$/\1/')
+    echo -e "$SAMPLE\tcase\t$i" >> "results/EHdn_prep/manifest.tsv"
 done 
